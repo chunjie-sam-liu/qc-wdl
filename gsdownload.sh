51b24edbe1e40782da718aec7b72b6b9d12a3898
chunjie-sam-liu <chunjie-sam-liu@foxmail.com>
2022-12-05 16:45:47 -0500

    new download

--- gsdownload.sh
+++ gsdownload.sh
@@ -6,18 +6,34 @@
 
 # Number of input parameters
 
-bucket="fc-secure-f4731dee-c700-4b23-9cd7-119292a806f1"
-sbid="3927dd42-bae5-48ec-af21-8f67b7dd1f6c"
-outdir="/home/liuc9/github/qc-wdl/data"
+# download QC output file
+# bucket="fc-secure-f4731dee-c700-4b23-9cd7-119292a806f1"
+# sbid="3927dd42-bae5-48ec-af21-8f67b7dd1f6c"
+# outdir="/home/liuc9/github/qc-wdl/data"
 
-gzfiles=$(gsutil ls gs://${bucket}/${sbid}/QC/*/call-gatherbam/*.tar.gz)
+# gzfiles=$(gsutil ls gs://${bucket}/${sbid}/QC/*/call-gatherbam/*.tar.gz)
+# for gz in ${gzfiles}
+# do
+#   bname=$(basename ${gz})
+#   [[ -f ${outdir}/${bname} ]] && echo "${bname} already exists!" && continue
+#   # echo ${gz}
+#   cmd="gsutil cp ${gz} ${outdir}/${bname}"
+#   echo ${cmd}
+#   eval ${cmd}
+# done
+
+
+## Download samtools idxstats file.
+bucket="fc-secure-d02ae31a-ab6d-4003-9904-3f5a1a03453c"
+submissionid="efad9daa-1671-4ba6-906a-5b281e70e6f8"
+outdir="/home/liuc9/scratch/mitochondrial/GTExv8-reads-ratio/GTExv8-idxstats"
+
+gzfiles=$(gsutil ls gs://${bucket}/submissions/${submissionid}/IDXSTATS/*/call-gather_idxstats/*.tar.gz)
 for gz in ${gzfiles}
 do
-  bname=$(basename ${gz})
-  [[ -f ${outdir}/${bname} ]] && echo "${bname} already exists!" && continue
-  # echo ${gz}
-  cmd="gsutil cp ${gz} ${outdir}/${bname}"
-  echo ${cmd}
-  eval ${cmd}
+    bname=$(basename ${gz})
+    [[ -f ${outdir}/${bname} ]] && echo "${bname} already exists!" && continue
+    cmd="gsutil cp ${gz} ${outdir}/${bname}"
+    echo ${cmd}
+    eval ${cmd}
 done
-
